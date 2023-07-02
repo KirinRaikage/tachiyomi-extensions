5e8d59566f2a06bac07ba4d3462cbeef559ac85b
KirinRaikage <manmitsu@hotmail.fr>
2023-07-02 16:53:17 +0200

    Add new status text for French Madara sources

--- multisrc/overrides/madara/bluesolo/src/BlueSolo.kt
+++ multisrc/overrides/madara/bluesolo/src/BlueSolo.kt
@@ -8,17 +8,4 @@ import java.util.Locale
 
 class BlueSolo : Madara("Blue Solo", "https://www1.bluesolo.org", "fr", dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.FRENCH)) {
     override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Statut) + .summary-content"
-
-    private fun parseStatus(status: String?) = when {
-        status == null -> SManga.UNKNOWN
-        status.contains("En cours de publication", ignoreCase = true) -> SManga.ONGOING
-        status.contains("Achevé", ignoreCase = true) -> SManga.COMPLETED
-        else -> SManga.UNKNOWN
-    }
-
-    override fun mangaDetailsParse(document: Document): SManga {
-        return super.mangaDetailsParse(document).apply {
-            status = parseStatus(document.select(mangaDetailsSelectorStatus).text())
-        }
-    }
 }
