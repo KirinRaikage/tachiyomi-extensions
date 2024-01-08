0caf31fa4bc6dbe0547f6a8fdb4ec8a73f750702
KirinRaikage <manmitsu@hotmail.fr>
2024-01-08 17:44:22 +0100

    Update Sushi Scans sources

--- multisrc/overrides/mangathemesia/sushiscanfr/src/SushiScanFR.kt
+++ multisrc/overrides/mangathemesia/sushiscanfr/src/SushiScanFR.kt
@@ -1,12 +1,20 @@
 package eu.kanade.tachiyomi.extension.fr.sushiscanfr
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.OkHttpClient
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
-class SushiScanFR : MangaThemesia("Sushiscan.fr", "https://anime-sama.me", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH)) {
+class SushiScanFR : MangaThemesia("Sushiscan.fr", "https://sushiscan.fr", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH)) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(2, 1, TimeUnit.SECONDS)
+        .build()
+
     override val altNamePrefix = "Nom alternatif : "
     override val seriesAuthorSelector = ".imptdt:contains(Auteur) i, .fmed b:contains(Auteur)+span"
     override val seriesStatusSelector = ".imptdt:contains(Statut) i"
@@ -14,6 +22,7 @@ class SushiScanFR : MangaThemesia("Sushiscan.fr", "https://anime-sama.me", "fr",
         this == null -> SManga.UNKNOWN
         this.contains("En Cours", ignoreCase = true) -> SManga.ONGOING
         this.contains("Terminé", ignoreCase = true) -> SManga.COMPLETED
+        this.contains("Abandonné", ignoreCase = true) -> SManga.CANCELLED
         else -> SManga.UNKNOWN
     }
 
